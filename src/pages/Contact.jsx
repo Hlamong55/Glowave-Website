b866fdbda9912173e2c856e9181db3757e5124de
Hlamong55 <bijoymarma55@gmail.com>
2026-04-23 22:28:08 +0600

    contact page fully updated with multi language support

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 import { FaPhoneAlt, FaEnvelope, FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 function Contact() {
+
+  const { t } = useTranslation();
+
   return (
     <div className="relative pt-32 pb-24 bg-linear-to-b from-blue-50 via-white to-blue-100 overflow-hidden">
       {/* floating blobs */}
@@ -24,7 +28,7 @@ function Contact() {
         {/* Title */}
 
         <h1 className="text-5xl font-bold text-center mb-16 text-blue-600">
-          Contact Us
+        {t("contact.title")}
         </h1>
 
         {/* Contact Info */}
@@ -33,15 +37,14 @@ function Contact() {
           {/* LEFT SIDE */}
 
           <div>
-            <p className="text-sm font-semibold text-blue-600 tracking-widest mb-2">
-              CONTACT US
+            <p className="text-sm font-semibold text-blue-600 tracking-widest mb-4">
+              {t("contact.title")}
             </p>
 
-            <h2 className="text-4xl font-bold mb-4">Get in touch today</h2>
+            <h2 className="text-4xl font-bold mb-4">{t("contact.subtitle")}</h2>
 
             <p className="text-gray-600 mb-8">
-              Have questions about our services? We'd love to hear from you.
-              Send us a message and our team will respond as soon as possible.
+              {t("contact.desc")}
             </p>
 
             {/* Email Card */}
@@ -52,7 +55,7 @@ function Contact() {
               </div>
 
               <div>
-                <p className="text-sm text-gray-500">Email</p>
+                <p className="text-sm text-gray-500">{t("contact.info.email")}</p>
                 <p className="font-medium">contact@glowave.com</p>
               </div>
             </div>
@@ -65,7 +68,7 @@ function Contact() {
               </div>
 
               <div>
-                <p className="text-sm text-gray-500">Phone</p>
+                <p className="text-sm text-gray-500">{t("contact.info.phone")}</p>
                 <p className="font-medium">(+880) 1818470577</p>
               </div>
             </div>
@@ -73,7 +76,7 @@ function Contact() {
             {/* Social Icons */}
 
             <div className="flex items-center gap-4">
-              <p className="text-gray-700 mr-2">Reach out to us:</p>
+              <p className="text-gray-700 mr-2">{t("contact.info.reach")}:</p>
 
               <div className="flex gap-3">
                 <a className="w-11 h-11 flex items-center justify-center rounded-full bg-gray-200 hover:bg-blue-600 hover:text-white transition">
@@ -97,25 +100,25 @@ function Contact() {
             <form className="grid gap-5">
               <input
                 type="text"
-                placeholder="Full Name"
+                placeholder={t("contact.form.name")}
                 className="bg-white border border-gray-300 rounded-lg p-3 focus:outline-none focus:border-blue-500"
               />
 
               <input
                 type="email"
-                placeholder="Email"
+                placeholder={t("contact.form.email")}
                 className="bg-white border border-gray-300 rounded-lg p-3 focus:outline-none focus:border-blue-500"
               />
 
               <input
                 type="text"
-                placeholder="Company (optional)"
+                placeholder={t("contact.form.company")}
                 className="bg-white border border-gray-300 rounded-lg p-3 focus:outline-none focus:border-blue-500"
               />
 
               <textarea
                 rows="4"
-                placeholder="Leave us a message"
+                placeholder={t("contact.form.message")}
                 className="bg-white border border-gray-300 rounded-lg p-3 focus:outline-none focus:border-blue-500"
               />
 
@@ -125,7 +128,7 @@ function Contact() {
                 type="submit"
                 className="bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 hover:scale-105 transition font-medium"
               >
-                Send Message
+                {t("contact.form.button")}
               </button>
             </form>
           </div>
@@ -135,7 +138,7 @@ function Contact() {
 
         <div className="mt-20">
           <p className="text-2xl font-semibold text-blue-600 tracking-widest mb-5">
-              📍Our Location
+              📍{t("contact.info.location")}
             </p>
           <iframe
             title="map"
